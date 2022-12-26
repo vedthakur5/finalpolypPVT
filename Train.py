55aea4245fcb7229785a15148b1934e0a6104b47
Ved Thakur <thakur.6@iitj.ac.in>
2022-12-26 16:30:49 +0530

    Update Train.py

--- Train.py
+++ Train.py
@@ -120,9 +120,9 @@ def train(train_loader, model, optimizer, epoch, test_path):
             beta = 2
             loss_P1 = structure_loss(P1, gts)
             loss_P2 = structure_loss(P2, gts)
-            loss_P1 = loss_P1.cpu().detach().numpy()
-            loss_P2 = loss_P2.cpu().detach().numpy()
-            loss_p1p2 = np.square(np.subtract(loss_P1, loss_P2)).mean()
+            nloss_P1 = loss_P1.cpu().detach().numpy()
+            nloss_P2 = loss_P2.cpu().detach().numpy()
+            loss_p1p2 = np.square(np.subtract(nloss_P1, nloss_P2)).mean().item()
             loss = loss_P1 + alpha*loss_P2 + beta*loss_p1p2
             # ---- backward ----
             loss.backward()
